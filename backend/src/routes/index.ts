7e2520ab209f0290479c17f6c831974bd7c3d832
Jaden <jaden.kasche@gmail.com>
2025-11-27 20:53:18 +0100

    feat: added user-login in backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import mailRouter from "../email-service/routes/mailRoutes.js";
 import userRouter from "../user-service/routes/userRoutes.js";
+import login from "./users.js";
 
 const router = Router();
 
 // Mail Endpoints
 router.use("/mail", mailRouter);
 router.use("/users", userRouter);
+router.use("/login", login);
 
 export default router;
