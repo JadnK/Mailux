8b3f061e4bb2bfebff145242894ad35f6502d250
Jaden <jaden.kasche@gmail.com>
2026-01-19 15:35:21 +0100

    changed API_BASE_URL

--- frontend/src/api/settings.ts
+++ frontend/src/api/settings.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://jadenk.de:5000/api';
+const API_BASE_URL = 'https://mail.api.jadenk.de/api';
 
 export interface UserSettings {
   name: string;
